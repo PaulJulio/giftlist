aed1200b62e40159f8f77e29a2804a823ab89ad5
Paul Hulett <paulhulett@gmail.com>
2014-11-29 14:57:12 -0800

    List Calculation
    First list calculation code, can successfully create a linked list of givers while avoiding group collisions.

---
+++ gift/giver.php
@@ -0,0 +1,134 @@
+<?php
+namespace gift;
+
+class Giver {
+    /* @var string $name */
+    private $name;
+    /* @var string $email */
+    private $email;
+    /* @var int $group */
+    private $group;
+    /* @var Giver $giveTo */
+    private $giveTo;
+    /* @var Giver $getFrom */
+    private $getFrom;
+
+    /**
+     * @return string
+     */
+    public function getEmail() {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGroup() {
+        return $this->group;
+    }
+
+    /**
+     * @param int $group
+     */
+    public function setGroup($group) {
+        $this->group = $group;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    /**
+     * @return Giver
+     */
+    public function getGetFrom() {
+        return $this->getFrom;
+    }
+
+    /**
+     * @param $getFrom
+     * @throws \Exception
+     */
+    public function setGetFrom($getFrom) {
+        if ($this->getGroup() == $getFrom->getGroup()) {
+            throw new \Exception('Cannot get from member of same group');
+        }
+        $this->getFrom = $getFrom;
+    }
+
+    /**
+     * @return Giver
+     */
+    public function getGiveTo() {
+        return $this->giveTo;
+    }
+
+    /**
+     * @param $giveTo
+     * @throws \Exception
+     */
+    public function setGiveTo($giveTo) {
+        if ($this->getGroup() == $giveTo->getGroup()) {
+            throw new \Exception('Cannot give to member of same group');
+        }
+        $this->giveTo = $giveTo;
+    }
+
+    /**
+     * @param Giver $firstCalled
+     * @return $this|bool
+     *
+     * The head is the first giver. Returns true if the list is circular.
+     */
+    public function getHead(Giver $firstCalled = null) {
+        if (isset($firstCalled)) {
+            if ($firstCalled == $this) {
+                return true;
+            }
+        } else {
+            $firstCalled = $this;
+        }
+        if (isset($this->getFrom)) {
+            return $this->getFrom->getHead($firstCalled);
+        }
+        return $this;
+    }
+
+    /**
+     * @param Giver $firstCalled
+     * @return $this|bool
+     *
+     * The tail is the last recipient. Returns true if the list is circular.
+     */
+    public function getTail(Giver $firstCalled = null) {
+        if (isset($firstCalled)) {
+            if ($firstCalled == $this) {
+                return true;
+            }
+        } else {
+            $firstCalled = $this;
+        }
+        if (isset($this->giveTo)) {
+            return $this->giveTo->getTail($firstCalled);
+        }
+        return $this;
+    }
+
+}
---
+++ gift/main.php
@@ -0,0 +1,93 @@
+<?php
+namespace gift;
+require_once(__DIR__ . '/loader.php');
+
+$groups = 0; // members of a group can not buy a gift for another member of the group
+$list =  array(); // todo: allow for this list to be uploaded
+$hash = md5_file(__DIR__ . '/list.csv');
+$fh = fopen(__DIR__ . '/list.csv', 'r');
+while (($fl = fgetcsv($fh)) !== false) {
+    foreach($fl as $item) {
+        $info = explode(':', trim($item)); // The Name:email@tld.com
+        $giver = new Giver();
+        $giver->setName($info[0]);
+        $giver->setEmail($info[1]);
+        $giver->setGroup($groups);
+        $list[] = $giver;
+    }
+    ++$groups;
+}
+
+printf('Loaded %s givers in %s groups%s', count($list), $groups, PHP_EOL);
+$tries = 1;
+$success = false;
+while ($tries < 25) {
+    // seed the random number generator with 8 hex digits taken from the end of the md5 hash
+    // on each try, we shift the starting position left one character in the hash
+    $hex = '0x' . substr($hash, (-7 - $tries), 8);
+    printf('Seed for try #%s: %s%s', $tries++, $hex, PHP_EOL);
+    srand(hexdec($hex));
+    // get copy of our list by value
+    $l = unserialize(serialize($list));
+    $i = rand(0, count($l) - 1);
+    /* @var Giver $start */
+    $start = array_pop(array_splice($l, $i, 1));
+    while (count($l)) {
+        if (count($l) == 1) {
+            // the last element needs to have a different group than both the head and the tail
+            /* @var Giver $last */
+            $last = $l[0];
+            /* @var Giver $head */
+            $head = $start->getHead();
+            /* @var Giver $tail */
+            $tail = $start->getTail();
+            if ($head === true || $tail === true) {
+                throw new \Exception('Unexpected Circular List Before Last Item Added');
+            }
+            if ($last->getGroup() == $head->getGroup() || $last->getGroup() == $tail->getGroup()) {
+                printf('Could not insert %s between %s and %s because of group collision. Trying again.%s', $last->getName(), $head->getName(), $tail->getName(), PHP_EOL);
+                continue 2;
+            }
+            // ok, the list has been created
+            // todo: send emails. for now, I'm outputting the list
+            printf($head->getName() . ' -> ');
+            $next = $head;
+            while ($next = $next->getGiveTo()) {
+                printf($next->getName() . ' -> ');
+            }
+            printf('%s %s', $last->getName(), PHP_EOL);
+            continue 2;
+        }
+        $i = rand(0, count($l) - 1);
+        /* @var Giver $pick */
+        $pick = array_pop(array_splice($l, $i, 1));
+        $head = $start->getHead();
+        $tail = $start->getTail();
+        if ($head === true || $tail === true) {
+            throw new \Exception('Unexpected Circular List Before Last Item Added');
+        }
+        // first try to add pick to tail
+        if ($pick->getGroup() != $tail->getGroup()) {
+            $tail->setGiveTo($pick);
+            $pick->setGetFrom($tail);
+        } elseif ($pick->getGroup() != $head->getGroup()) {
+            $pick->setGiveTo($head);
+            $head->setGetFrom($pick);
+        } else {
+            // cannot insert into the chain, put it back at the end of the list
+            array_push($l, $pick);
+            // check to make sure that we have more than one group in the list
+            $allsame = true;
+            /* @var Giver $v */
+            foreach ($l as $v) {
+                if ($pick->getGroup() != $v->getGroup()) {
+                    $allsame = false;
+                }
+            }
+            if ($allsame) {
+                printf('Could not insert %s between %s an %s because of group collision, and all remaining list members are in the same group. Trying again.%s', $pick->getName(), $head->getName(), $tail->getName(), PHP_EOL);
+                continue 2;
+            }
+        }
+    }
+}
